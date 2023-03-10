2938fe75e8e54c4d94495271d5c0928736c5990a
parker4942 <parkmj4312@gmail.com>
2023-03-10 18:56:28 +0900

    spring 시큐리티 커밋

---
+++ src/main/java/com/sparta/springsequrity/entity/User.java
@@ -0,0 +1,32 @@
+package com.sparta.springsequrity.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private UserRoleEnum role;
+
+    public User(String username, String password, UserRoleEnum role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+}
---
+++ src/main/java/com/sparta/springsequrity/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.sparta.springsequrity.repository;
+
+import com.sparta.springsequrity.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+}
